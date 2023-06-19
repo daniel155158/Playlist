ddd1daef79e17c6de5bb7da824bcdd24499359c1
daniel155158 <daniel155158@gmail.com>
2023-06-19 13:46:55 +0800

    feat: create album model

---
+++ Playlist/Models/Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Playlist.Models
+{
+    public class Album
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        // one album has many songs
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
