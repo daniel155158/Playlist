5b2a85a5ea756da5098138d90c2cb9e6f95e620b
daniel155158 <daniel155158@gmail.com>
2023-06-19 21:28:23 +0800

    feat: update delete feature in SongController

--- Playlist/Controllers/SongController.cs
+++ Playlist/Controllers/SongController.cs
@@ -112,11 +112,15 @@ namespace Playlist.Controllers
         }
 
         // GET: Song/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
             }
             Song song = db.Songs.Find(id);
             if (song == null)
