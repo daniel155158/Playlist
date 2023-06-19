1157eb73492acb136e7668c24de89fbf98cda3bd
daniel155158 <daniel155158@gmail.com>
2023-06-19 13:37:57 +0800

    feat: create song model

---
+++ Playlist/Models/Song.cs
@@ -0,0 +1,15 @@
+﻿namespace Playlist.Models
+{
+    public class Song
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int SingerID { get; set; }
+        public int AlbumID { get; set; }
+
+        // one song belongs to one singer
+        public virtual Singer Singer { get; set; }
+        // one song belongs to one album
+        public virtual Album Album { get; set; }
+    }
+}
