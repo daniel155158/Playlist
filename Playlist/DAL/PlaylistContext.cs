73f1d1dafb7bbe76abadddd288ce557e535b53e5
daniel155158 <daniel155158@gmail.com>
2023-06-19 14:22:55 +0800

    feat: create database context: PlaylistContext

---
+++ Playlist/DAL/PlaylistContext.cs
@@ -0,0 +1,25 @@
+﻿using Playlist.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Playlist.DAL
+{
+    public class PlaylistContext : DbContext
+    {
+
+        // Specify the connection string
+        public PlaylistContext() : base("PlaylistContext")
+        { 
+        }
+
+        public DbSet<Singer> Singers { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Specify singular table names (避免table自動產生為複數名稱)
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
